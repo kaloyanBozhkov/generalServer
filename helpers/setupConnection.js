12444d520c9efd7bc977215d6137e8e0ac19c670
koko <koko.bozhkov@elementhuman.com>
2021-01-26 21:01:40 +0200

    changed back port for mysql conn

--- helpers/setupConnection.js
+++ helpers/setupConnection.js
@@ -2,7 +2,6 @@ const mysql = require('mysql')
 
 const connection = mysql.createPool({
   host: process.env.REACT_APP_HOST,
-  port: 3307,
   user: process.env.REACT_APP_USERNAME,
   password: process.env.REACT_APP_PASSWORD,
   database: process.env.REACT_APP_DATABASE,
